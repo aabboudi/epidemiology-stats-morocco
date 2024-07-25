553aa2fd3e55af8517bfd8c5cd7622c938295663
aabboudi <abboudi.abdellah@gmail.com>
2024-07-25 21:39:13 +0100

    Created Footer component

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center items-center text-gray-900 dark:text-gray-400 w-full h-12 bg-gray-400 dark:bg-gray-900">
+      <p>All rights reserved</p>
+    </div>
+  )
+}
