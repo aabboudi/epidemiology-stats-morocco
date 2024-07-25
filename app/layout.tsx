3657f26e6e5d3f73e86e1994ffb30d0af335b267
aabboudi <abboudi.abdellah@gmail.com>
2024-07-25 21:10:04 +0100

    Moved Navbar component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
+import Navbar from "@/components/navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +27,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Navbar from "@/components/navbar";
-
 export default function Home() {
   return (
-    <>
-    <Navbar />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       TEST
     </main>
-    </>
   );
 }
